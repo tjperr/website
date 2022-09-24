2c811ecfb1004e0d73beb781606034d1624e9349
tjperr <thomas_perrett@outlook.com>
2022-09-24 18:00:59 +0100

    add photo redirect page

---
+++ pages/photos.js
@@ -0,0 +1,19 @@
+import Layout, { siteTitle } from "../components/layout";
+
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+<html>
+   <head>
+      <title>Wei En and Tom Wedding Photos</title>
+      <meta httpEquiv = "refresh" content = "3; url = https://drive.google.com/drive/folders/1oNL7jpUGZhmIqdgtJ-CK07U7dXTJnkyl?usp=sharing" />
+   </head>
+   <body>
+   <h3>Wei En and Tom Wedding Photos</h3>
+      <p>Taking you to google drive...</p>
+   </body>
+</html>
+  );
+}
